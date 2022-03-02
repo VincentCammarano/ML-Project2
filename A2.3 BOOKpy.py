717fcd34b39c17f2272adac49e58980b44364026
VincentC <learntomaster98@gmail.com>
2022-03-02 20:50:53 +0100

    incorrect on test and training set

--- A2.3 BOOKpy.py
+++ A2.3 BOOKpy.py
@@ -25,37 +25,52 @@ def perceptron(x, y, b, w_init, eta, epoch):
     x.insert(0, "1", 1, True)
     x = x.to_numpy()
 
+    for i in range(x.shape[0]):
+        if y[i] == 0:
+            x[i] = -x[i]
+
+
     for k in range(epoch):
 
         i = (k % x.shape[0])
         xi = x[i]
 
         #print(w[1:].T @ xi)
-        if y[i] == 0:
-            print(w.T @ xi)
-            dot_product = w.T @ xi
-            if(w.T @ xi) >= 0 and (w.T @ xi) <= b:
-                #print("Classified correctly with too small margin")
-                update = np.multiply(eta * k, xi)
-                #update = update.reshape((update.shape[0], 1))
-                w += update
-                #print((eta * k))
-                #print((eta * k)  * xi)
-
-        if y[i] == 1:
-            print(w.T @ xi)
-            dot_product =w.T @ xi
-            if(w.T @ xi) <= 0 and (w.T @ xi) <= b:
-                update = np.multiply(eta * k,  xi)
-                #update = update.reshape((update.shape[0], 1))
-                w += update
-
-
+        print(w.T @ xi)
+        #dot_product = w.T @ xi
+        pred = w.T @ xi
+        if (pred) <= b:
+            #print("Classified correctly with too small margin")
+            update = np.multiply(eta * k, ( y[i] - pred))
+            w += update
+            #print((eta * k))
+            #print((eta * k)  * xi)
+        elif (w.T @ xi) > b:
+            return w
 
     return w
 
 
 w_init = np.zeros([x.shape[1], 1])
 w_init = np.append(1, w_init)
-p = perceptron(x, y.to_numpy(), 2, w_init=w_init, eta=0.1, epoch=10)
+p = perceptron(x, y.to_numpy(), 1, w_init=w_init, eta=0.1, epoch=10)
 print(p)
+print(p.shape)
+dot = p.T
+print(dot.shape)
+def predict(w, x):
+
+    x = x.to_numpy()
+
+    pred = x @ w
+    print(pred)
+    for i in range (len(pred)):
+        if pred[i] > 0:
+            pred[i] = 1
+        elif pred[i] < 0:
+            pred[i] = 0
+    return pred
+
+pred = predict(p, x)
+print("\n")
+print(type(pred))
